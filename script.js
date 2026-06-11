006ac0f768e3cf94e08f84802b655c205964b2d2
サラダオアシス <xiang669900@gmail.com>
2026-06-11 16:38:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const button = document.querySelector("button");
 const titleInput = document.getElementById("title");
 const categorySelect = document.getElementById("category");
+const statusSelect = document.getElementById("status");
 const list = document.getElementById("list");
 
 button.addEventListener("click", () => {
     const title = titleInput.value;
-    const category = categorySelect.value;
+const category = categorySelect.value;
+const status = statusSelect.value;
 
     if (!title) return;
 
     const item = document.createElement("div");
 
     const text = document.createElement("span");
-    text.textContent = `${title} (${category}) `;
+  text.textContent =
+`${title} (${category}) - ${status} `;
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "❌";
